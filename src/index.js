2a4e67be5a6c3362b90a5a8747f900958c290469
FrancoRosa <km115.franco@gmail.com>
2020-08-07 18:00:48 -0500

    Add style to main images

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import './bulma.min.css'
-import './style.css'
+import './bulma.min.css';
+import './style.css';
+import dish1 from './joseph-gonzalez-fdlZBWIP0aM-unsplash.jpg';
+import dish2 from './victoria-shes-4acsnIWXm3k-unsplash.jpg';
+import dish3 from './victoria-shes-COYAmXroY1A-unsplash.jpg';
 console.log('WebpackTest')
